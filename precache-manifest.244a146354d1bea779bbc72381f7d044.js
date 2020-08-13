aa7001a1f90b69f9c05dcd3d3ad8789ac28df5fc
richCode <chineduogada@gmail.com>
2020-08-13 18:05:14 +0100

    Updates

---
+++ precache-manifest.244a146354d1bea779bbc72381f7d044.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9eb6fae708278d7113f6050e9a1dbfe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ac40f6a2585a37dbd9e6",
+    "url": "./static/css/main.adf6e425.chunk.css"
+  },
+  {
+    "revision": "d0d656d8ecbd5abdc9fc",
+    "url": "./static/js/2.6d4a5d52.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "./static/js/2.6d4a5d52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac40f6a2585a37dbd9e6",
+    "url": "./static/js/main.38c53f09.chunk.js"
+  },
+  {
+    "revision": "2a0eefc6f8a25695471b",
+    "url": "./static/js/runtime-main.59edda60.js"
+  }
+]);
